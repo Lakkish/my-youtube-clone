4d8116bb09757830aa9f926e2a00e3184061913f
Lakkish <d.lashore2@gmail.com>
2025-06-26 19:41:34 +0530

    Refactor App component to include NxtWatchProvider for context management

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
+import { NxtWatchProvider } from "./context/NxtWatchContext";
+import "./App.css";
 
-import './App.css'
+const App = () => {
+  return (
+    <NxtWatchProvider>
+      <h1>Vite + React</h1>
+    </NxtWatchProvider>
+  );
+};
 
-const App = () => (<h1>Vite + React</h1> )
-
-export default App
+export default App;
