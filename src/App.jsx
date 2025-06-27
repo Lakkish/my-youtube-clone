c4beef7f54f32e4fc2cd6ad09d7c75f825dc090a
Lakkish <d.lashore2@gmail.com>
2025-06-27 09:20:12 +0530

    Update App component: add ProtectedRoute and Home route for main page access

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { Routes, Route } from "react-router-dom";
-
+import ProtectedRoute from "./components/ProtectedRoute";
+import Home from "./components/Home";
 import Login from "./components/Login";
-import { NxtWatchProvider } from "./context/NxtWatchContext";
+import { NxtWatchProvider } from "./context/NxtWatchContext.jsx";
 import "./App.css";
 
 const App = () => {
@@ -9,6 +10,14 @@ const App = () => {
     <NxtWatchProvider>
       <Routes>
         <Route path="/login" element={<Login />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </NxtWatchProvider>
   );
