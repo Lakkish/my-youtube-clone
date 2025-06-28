1925537b30c7a39d8c1ed0c42bd51b731a546efa
Lakkish <d.lashore2@gmail.com>
2025-06-28 16:11:46 +0530

    Add LoaderView component with dynamic theme-based loading spinner

---
+++ src/components/Loader/index.jsx
@@ -0,0 +1,15 @@
+import { useContext } from "react";
+import { ClipLoader } from "react-spinners";
+import NxtWatchContext from "../../context/NxtWatchContext";
+
+const LoaderView = () => {
+  const { isLightTheme } = useContext(NxtWatchContext);
+
+  return (
+    <LoaderContainer data-testid="loader">
+      <ClipLoader color={isLightTheme ? "#000000" : "#ffffff"} size={50} />
+    </LoaderContainer>
+  );
+};
+
+export default LoaderView;
